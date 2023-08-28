5a11cc8737a507569703aeb42d87b44804b10840
NelsonAMR <nelson.macias99@gmail.com>
2023-08-27 23:56:18 -0600

    update movie model

--- server/src/models/Movie.ts
+++ server/src/models/Movie.ts
@@ -6,6 +6,10 @@ const movieSchema = new Schema(
       type: String,
       required: true,
     },
+    image: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
